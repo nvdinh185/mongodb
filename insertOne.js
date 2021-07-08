1bc111481fb0c50b69010828725bd60b03713899
nvdinh09thxd <nvdinh09thxd@gmail.com>
2021-07-08 19:15:27 +0700

    test created_time

--- insertOne.js
+++ insertOne.js
@@ -4,7 +4,7 @@ var url = "mongodb://localhost:27017/";
 MongoClient.connect(url, function (err, db) {
   if (err) throw err;
   var dbo = db.db("mydb");
-  var myobj = { username: "nvdinh123", password: "123" };
+  var myobj = { username: "nvdinh123", password: true, created_time: new Date().getTime() };
   dbo.collection("customers").insertOne(myobj, function (err, res) {
     if (err) throw err;
     console.log("1 document inserted");
